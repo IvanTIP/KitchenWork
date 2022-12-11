7500e065f8f4dfeb1c14b0472545824ffde68bfe
IvanTIP <tipivanp@gmail.com>
2022-12-11 14:44:53 +0300

    release v1.2

--- main.cpp
+++ main.cpp
@@ -6,72 +6,43 @@
 
 std::mutex access1;
 std::mutex access2;
-std::mutex access3;
-
-class Courier {
-private:
-    //Delivery* delivery = new Delivery;
-public:
-    void sendingOrder (std::string &dish, int inI) {
-        std::this_thread::sleep_for(std::chrono::seconds(30));
-        std::cout << inI << " Courier get " << dish << std::endl;
-    }
-};
 
 class Delivery {
   private:
-    //Courier* courier = new Courier;
     std::vector<std::string> finishedDish;
     int index = 0;
-    bool status = false;
   public:
-    Delivery () {
+    Delivery() {
         for (int i = 0;i < 4;i++) {
-            finishedDish[i] = "unknown";
-        }
-    }
-
-
-    void getToCourier () {
-        for (int i = 0;i < 4;) {
-            if (finishedDish[index] != "unknown") {
-                courier->sendingOrder(finishedDish[index], i);
-                i++;
-            }
+            finishedDish.emplace_back("unknown");
         }
     }
 
     void setDish (std::string &dish) {
-        std::cout << index << " Delivery get " << dish << std::endl;
+        std::cout << "Position " << index << ": delivery get finished dish - " << dish << std::endl;
         finishedDish[index] = dish;
         index++;
-        //finishedDish.push_back(dish);
-        access1.lock();
-        status = true;
-        access1.unlock();
-    }
-
-    bool getStatus () {
-        return status;
-    }
-
-    void setStatus (bool inStatus) {
-        status = inStatus;
-    }
-
-     std::string getDish (int inI) {
-        return finishedDish[inI];
     }
 };
 
-
+class Courier {
+public:
+    void sendingOrder (std::vector<std::string> &dishList) {
+        for (int i = 0;i < 4;) {
+            if (dishList[i] != "unknown") {
+                std::this_thread::sleep_for(std::chrono::seconds(30));
+                std::cout << "Position " << i << ": courier get order. Sending to the customer dish - " << dishList[i] << std::endl;
+                i++;
+            }
+        }
+    }
+};
 
 class Kitchen {
 private:
     Delivery* delivery = new Delivery;
     bool status = false;
     std::string dish;
-    int index = 0;
 public:
     bool getStatus () {
         return status;
@@ -80,101 +51,110 @@ public:
     void setDish (std::string inDish, int ind) {
         dish = inDish;
         show(ind);
-        access3.lock();
+        access2.lock();
         status = true;
-        access3.unlock();
+        access2.unlock();
         cooking(ind);
-
     }
 
     void show (int i) {
-        std::cout << i << " Kitchen get " << dish << std::endl;
+        std::cout << "Position " << i << ": kitchen get dish - " << dish << std::endl;
     }
 
     void cooking (int i) {
-
-        std::cout << i << " Kitchen cooking " << dish << std::endl;
+        std::cout << "Position " << i << ": kitchen cooking dish - " << dish << std::endl;
         std::this_thread::sleep_for(std::chrono::seconds(std::rand() % 16 + 5));
-        std::cout << i << " Kitchen cooked " << dish << std::endl;
-
-
-        std::cout << i << " Kitchen sent to delivery " << dish << std::endl;
-        access3.lock();
+        std::cout << "Position " << i << ": kitchen cooked dish - " << dish << std::endl;
+        std::cout << "Position " << i << ": kitchen sent to delivery dish - " << dish << std::endl;
+        access2.lock();
         status = false;
+        access2.unlock();
         delivery->setDish(dish);
-        access3.unlock();
+    }
+
+    void clear () {
+        delete delivery;
     }
 };
 
 class Waiter {
 private:
     Kitchen* kitchen = new Kitchen;
-    std::vector<std::string> dish;
-    bool dishAdd = false;
     int index = 0;
 public:
-    void setDish (int inDish) {
-        access2.lock();
+    void setDish (int inDish, std::vector<std::string> &dishList) {
+        access1.lock();
         if (inDish == 0) {
-            dish.emplace_back("pizza");
-            dishAdd = true;
-            show ();
+            dishList[index]  = "pizza";
+            show (dishList);
         } else if (inDish == 1) {
-            dish.emplace_back("soup");
-            dishAdd = true;
-            show ();
+            dishList[index]  = "soup";
+            show (dishList);
         } else if (inDish == 2) {
-            dish.emplace_back("steak");
-            dishAdd = true;
-            show ();
+            dishList[index]  = "steak";
+            show (dishList);
         } else if (inDish == 3) {
-            dish.emplace_back("salad");
-            dishAdd = true;
-            show ();
+            dishList[index]  = "salad";
+            show (dishList);
         } else {
-            dish.emplace_back("sushi");
-            dishAdd = true;
-            show ();
+            dishList[index]  = "sushi";
+            show (dishList);
         }
-        access2.unlock();
+        access1.unlock();
     }
 
-    void show () {
-        std::cout << index << " The waiter received the order: " << dish[dish.size() - 1] << std::endl;
+    void show (std::vector<std::string> &dishList) {
+        std::cout << "Position " << index << ": the waiter received the order - " << dishList[index] << std::endl;
+        index++;
     }
 
-    void sendingOrder () {
-        while (index < 4) {
-            if (dishAdd && !kitchen->getStatus()) {
-                std::cout << index << " Waiter sent  to kitchen " << dish[index] << std::endl;
-                kitchen->setDish(dish[index], index);
-                index++;
-                dishAdd = false;
+    void sendingOrder (std::vector<std::string> &dishList) {
+        for (int i = 0;i < 4;) {
+            if (dishList[i] != "unknown" && !kitchen->getStatus()) {
+                std::cout << "Position " << i << ": waiter sent to kitchen dish - " << dishList[i] << std::endl;
+                kitchen->setDish(dishList[i], i);
+                i++;
             }
         }
     }
 
+    void clear () {
+        kitchen->clear();
+        delete kitchen;
+    }
+
+
 };
 
-void orderAcceptance (Waiter* waiter) {
+void orderAcceptance (Waiter* waiter, std::vector<std::string> &dishList) {
     for (int i = 0;i < 4;i++) {
         std::srand(std::time(nullptr));
         int t = std::rand() % 10 + 5;
-        std::cout << i << " Time " << t << std::endl;
+        std::cout << "Position " << i << ": order waiting time - " << t << std::endl;
         std::this_thread::sleep_for(std::chrono::seconds(t));
-        int d = std::rand() % 5;
-        std::cout << i << " Dish num " << d << std::endl;
-        waiter->setDish(d);
+        int num = std::rand() % 5;
+        std::cout << "Position " << i << ": order number - " << num << std::endl;
+        waiter->setDish(num, dishList);
+    }
+}
+
+void dishListInit (std::vector<std::string> &dishList) {
+    for (int i = 0;i < 4;i++) {
+        dishList.emplace_back("unknown");
     }
 }
 
 int main() {
+    std::vector<std::string> dishList;
+    dishListInit (dishList);
     Waiter* waiter = new Waiter;
-    //Courier* courier = new Courier;
-    std::thread th1(orderAcceptance, std::ref(waiter));
-    std::thread th2(&Waiter::sendingOrder, waiter);
-    //std::thread th3(&Kitchen::sentDish, kitchen);
-    std::thread th3(&Delivery::getToCourier, delivery);
+    Courier* courier = new Courier;
+    std::thread th1(orderAcceptance, std::ref(waiter), std::ref(dishList));
+    std::thread th2(&Waiter::sendingOrder, waiter, std::ref(dishList));
+    std::thread th3(&Courier::sendingOrder, courier, std::ref(dishList));
+    waiter->clear();
+    delete waiter;
+    delete courier;
     th1.join();
     th2.join();
     th3.join();

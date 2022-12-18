2b31dc093997461aad0f5bea5adcf876fa49ada5
IvanTIP <tipivanp@gmail.com>
2022-12-18 23:06:14 +0300

    release v2.1

--- main.cpp
+++ main.cpp
@@ -4,15 +4,14 @@
 #include <vector>
 #include <mutex>
 
-std::mutex access1;
-std::mutex access2;
+std::mutex access;
 
 void orderReceipt (std::vector<std::string> &order) {
-    for (int i = 0;i < 4;i++) {
+    for (int i = 0;i < 10;i++) {
         std::srand(std::time(nullptr));
         std::this_thread::sleep_for(std::chrono::seconds(std::rand() % 10 + 5));
         int dishNum = std::rand() % 5;
-        access2.lock();
+        access.lock();
         if (dishNum == 0) {
             order[i] = "pizza";
             std::cout <<  "The waiter accepted the order: pizza" << std::endl;
@@ -29,44 +28,46 @@ void orderReceipt (std::vector<std::string> &order) {
             order[i] = "sushi";
             std::cout << "The waiter accepted the order: sushi" << std::endl;
         }
-        access2.unlock();
+        access.unlock();
     }
 }
 
 void kitchen (std::vector<std::string> &order, std::vector<std::string> &finishedDish) {
-    for (int i = 0;i < 4;) {
-        access2.lock();
+    for (int i = 0;i < 10;) {
+        access.lock();
         if (order[i] != "unknown") {
+            access.unlock();
             std::cout << "The kitchen accepted the order: " << order[i] << std::endl;
             std::cout << "Cooking " << order[i] << std::endl;
-            access2.unlock();
             std::srand(std::time(nullptr));
             std::this_thread::sleep_for(std::chrono::seconds(std::rand() % 15 + 5));
-            access2.lock();
             std::cout << "The " << order[i] << " is ready" << std::endl;
+            access.lock();
             finishedDish[i] = order[i];
-            access2.unlock();
+            access.unlock();
             i++;
         } else {
-            access2.unlock();
+            access.unlock();
         }
     }
 }
 
 void courier (std::vector<std::string> &finishedDish) {
-    for (int i = 0;i < 4;) {
-        access2.lock();
+    for (int i = 0;i < 10;) {
+        access.lock();
         if (finishedDish[i] != "unknown") {
+            access.unlock();
             std::this_thread::sleep_for(std::chrono::seconds(30));
             std::cout << "The courier accepted the order: " << finishedDish[i] << std::endl;
             i++;
+        } else {
+            access.unlock();
         }
-        access2.unlock();
     }
 }
 
 void dishListsInit (std::vector<std::string> &order, std::vector<std::string> &finishedDish) {
-    for (int i = 0;i < 4;i++) {
+    for (int i = 0;i < 10;i++) {
         order.emplace_back("unknown");
         finishedDish.emplace_back("unknown");
     }
@@ -75,16 +76,12 @@ void dishListsInit (std::vector<std::string> &order, std::vector<std::string> &f
 int main() {
     std::vector<std::string> order;
     std::vector<std::string> finishedDish;
-    access1.lock();
     dishListsInit(order, finishedDish);
-    access1.unlock();
-    access1.lock();
     std::thread th1(orderReceipt, std::ref(order));
     std::thread th2(kitchen, std::ref(order), std::ref(finishedDish));
     std::thread th3(courier, std::ref(finishedDish));
     th1.join();
     th2.join();
     th3.join();
-    access1.unlock();
     return 0;
 }
